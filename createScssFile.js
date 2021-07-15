4a840dc8b22dc43ed0380f1ccd70555e850e256d
上田貴央 <gibson511511@gmail.com>
2021-07-15 17:53:54 +0900

    ts導入（暫定）

--- createScssFile.js
+++ createScssFile.js
@@ -2,74 +2,74 @@ const fs = require('fs');
 const prefix = process.argv[2];
 const fileName = process.argv[3];
 const errorMessage = `Please enter "l", "c", "p" or "u" for 2nd argument
-      "l": layout,
-      "c": component,
-      "p": project,
-      "u": utility`;
+		"l": layout,
+		"c": component,
+		"p": project,
+		"u": utility`;
 let dirName = '';
 
 /**
  * @summary 新しいscssファイルを作成する
  */
 const createNewScssFile = () => {
-  const temp = `@use "../${prefix !== 'l' ? '../' : '' }foundation/var.scss";
+	const temp = `@use "../${prefix !== 'l' ? '../' : '' }foundation/var.scss";
 
 .${prefix}-${fileName} {
 
 }
 `;
 
-  fs.writeFileSync(`src/scss/${dirName}/_${fileName}.scss`, temp, 'utf8');
+	fs.writeFileSync(`src/scss/${dirName}/_${fileName}.scss`, temp, 'utf8');
 };
 
 /**
  * @summary index.scssファイルに"@forward"を追記する
  */
 const appendIndexFile = () => {
-  fs.appendFileSync(`src/scss/${dirName}/index.scss`, `\n@forward "${fileName}";`, 'utf8');
+	fs.appendFileSync(`src/scss/${dirName}/index.scss`, `\n@forward "${fileName}";`, 'utf8');
 };
 
 
 try {
-  /**
-   * @summary 入力されたprefixごとにディレクトリ名を決定
-   */
-  switch(true) {
-    case(prefix === 'l'):
-      dirName = 'layout';
-      break;
+	/**
+	 * @summary 入力されたprefixごとにディレクトリ名を決定
+	 */
+	switch (true) {
+		case (prefix === 'l'):
+			dirName = 'layout';
+			break;
 
-    case(prefix === 'c'):
-      dirName = 'component';
-      break;
+		case (prefix === 'c'):
+			dirName = 'component';
+			break;
 
-    case(prefix === 'p'):
-      dirName = 'project';
-      break;
+		case (prefix === 'p'):
+			dirName = 'project';
+			break;
 
-    case(prefix === 'u'):
-      dirName = 'utility';
-      break;
+		case (prefix === 'u'):
+			dirName = 'utility';
+			break;
 
-    default:
-      throw Error(errorMessage);
-  }
+		default:
+			throw Error(errorMessage);
+	}
 
-  /**
-   * @summary layout以外の場合パスに'object/'を追加
-   */
-  if(dirName !== 'layout') dirName = `object/${dirName}`;
+	/**
+	 * @summary layout以外の場合パスに'object/'を追加
+	 */
+	if (dirName !== 'layout') dirName = `object/${dirName}`;
 
-  /**
-   * @summary 既に同名のファイルが存在する場合エラー
-   */
-  if(fs.existsSync(`src/scss/${dirName}/_${fileName}.scss`)) {
-    throw Error(`src/scss/${dirName}/_${fileName}.scss already exits!`);
-  }
+	/**
+	 * @summary 既に同名のファイルが存在する場合エラー
+	 */
+	if (fs.existsSync(`src/scss/${dirName}/_${fileName}.scss`)) {
+		throw Error(`src/scss/${dirName}/_${fileName}.scss already exits!`);
+	}
 
-  createNewScssFile();
-  appendIndexFile();
+	createNewScssFile();
+	appendIndexFile();
 
 } catch (err) {
-  console.error(err);
+	console.error(err);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,160 +9,177 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 // ejsファイルが増えても問題ないような処置
 const assignPlugins = (env_mode) => {
-  const srcFiles = globule.find(['**/*.ejs', '!**/_*.ejs'], {cwd: `${__dirname}/src/ejs`});
-  const entriesList = {};
-  const assignObject = {plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        'assets/stylesheet',
-        'assets/javascript',
-      ]
-    }),
-    new MiniCssExtractPlugin({
-      filename: env_mode
-        ? 'assets/stylesheet/bundle.css'
-        : 'assets/stylesheet/bundle.[contenthash].css',
-    }),
-    //なんかいいヤツらしい。
-    new HtmlWebpackHarddiskPlugin({
-      alwaysWriteToDisk: true
-    }),
-    // 画像をassets/images/にコピー
-    new CopyPlugin({
-      patterns: [
-        {from: "src/images/", to: "assets/images/"}
-      ]
-    })
-  ]};
+	const srcFiles = globule.find(['**/*.ejs', '!**/_*.ejs'], {
+		cwd: `${__dirname}/src/ejs`
+	});
+	const entriesList = {};
+	const assignObject = {
+		plugins: [
+			new CleanWebpackPlugin({
+				cleanOnceBeforeBuildPatterns: [
+					'assets/stylesheet',
+					'assets/javascript',
+				]
+			}),
+			new MiniCssExtractPlugin({
+				filename: env_mode ?
+					'assets/stylesheet/bundle.css' :
+					'assets/stylesheet/bundle.[contenthash].css',
+			}),
+			//なんかいいヤツらしい。
+			new HtmlWebpackHarddiskPlugin({
+				alwaysWriteToDisk: true
+			}),
+			// 画像をassets/images/にコピー
+			new CopyPlugin({
+				patterns: [{
+					from: "src/images/",
+					to: "assets/images/"
+				}]
+			})
+		]
+	};
 
-  for(const ejsFileName of srcFiles) {
-    const htmlFileName = ejsFileName.replace(new RegExp(`.ejs`, 'i'), `.html`);
-    entriesList[htmlFileName] = `${__dirname}/src/ejs/${ejsFileName}`;
-  }
+	for (const ejsFileName of srcFiles) {
+		const htmlFileName = ejsFileName.replace(new RegExp(`.ejs`, 'i'), `.html`);
+		entriesList[htmlFileName] = `${__dirname}/src/ejs/${ejsFileName}`;
+	}
 
-  for(const [htmlFileName, ejsFileName] of Object.entries(entriesList)) {
-    assignObject.plugins.push(new HtmlWebpackPlugin({
-      filename : htmlFileName,
-      template : ejsFileName,
-      // 指定しないとjsもhead内に書かれちゃう
-      inject: 'body',
-      // prod時にコメントだけ削除する
-      minify: env_mode ?
-        false :
-        {
-          collapseWhitespace: false,
-          removeComments: true,
-        },
-    }));
-  }
+	for (const [htmlFileName, ejsFileName] of Object.entries(entriesList)) {
+		assignObject.plugins.push(new HtmlWebpackPlugin({
+			filename: htmlFileName,
+			template: ejsFileName,
+			// 指定しないとjsもhead内に書かれちゃう
+			inject: 'body',
+			// prod時にコメントだけ削除する
+			minify: env_mode ?
+				false : {
+					collapseWhitespace: false,
+					removeComments: true,
+				},
+		}));
+	}
 
-  return assignObject;
+	return assignObject;
 };
 
 module.exports = (env, argv) => {
-  const is_DEVELOP = argv.mode === "development";
+	const is_DEVELOP = argv.mode === "development";
 
-  return [
-    Object.assign({
-      entry: './src/js/main',
-      output: {
-        path: path.join(__dirname, 'public'),
-        filename: is_DEVELOP
-          ? 'assets/javascript/bundle.js'
-          : 'assets/javascript/bundle.[contenthash].js',
-      },
-      devtool: is_DEVELOP ? 'source-map' : 'eval',
-      watchOptions: {
-        ignored: /node_modules/
-      },
-      resolve: {
-        // 拡張子を省略してimportできるようになる
-        extensions: ['.js', '.ts'],
-      },
+	return [
+		Object.assign({
+			entry: {
+				mainJs: './src/js/main',
+				mainTs: './src/ts/main'
+			},
+			output: {
+				path: path.join(__dirname, 'public'),
+				filename: is_DEVELOP ?
+					'assets/javascript/[name].js' :
+					'assets/javascript/[name].[contenthash].js',
+			},
+			devtool: is_DEVELOP ? 'source-map' : 'eval',
+			watchOptions: {
+				ignored: /node_modules/
+			},
+			resolve: {
+				// 拡張子を省略してimportできるようになる
+				extensions: ['.js', '.ts'],
+			},
 
-      // UglifyJSPluginは非推奨
-      optimization: {
-        minimize: is_DEVELOP ? false : true,
-        minimizer: [
-          new TerserPlugin({
-            terserOptions: {
-              ecma: 2020,
-            }
-          })
-        ]
-      },
+			// UglifyJSPluginは非推奨
+			optimization: {
+				minimize: is_DEVELOP ? false : true,
+				minimizer: [
+					new TerserPlugin({
+						terserOptions: {
+							ecma: 2020,
+						}
+					})
+				]
+			},
 
-      module: {
-        rules: [
-          {
-            test: /\.ejs$/,
-            use: 'ejs-compiled-loader',
-          },
-          {
-            test: /\.scss$/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader',
-                options: {
-                  url: false,// sassで相対パスを書けるようにする
-                  sourceMap: true,
-                },
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: {
-                    plugins: [
-                      [
-                        require('autoprefixer')({grid: true}),
-                      ],
-                    ],
-                  }
-                }
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                  implementation: require('sass'),
-                },
-              },
-            ]
-          },
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: 'babel-loader',
-                options: {
-                  // promiseを使えるようにするヤツ
-                  presets: ['@babel/preset-env', '@babel/preset-react'],
-                  plugins: ['@babel/plugin-transform-runtime'],
-                },
-              },
-            ],
-          },
-          {
-            enforce: 'pre',
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'eslint-loader',
-          },
-        ],
-      },
-      devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        port: 8080,
-      },
-      stats: {
-        children: true //現状機能していない模様
-      },
-      target: is_DEVELOP ?
-        // dev-serverのホットリロードが効かない問題への対処
-        "web" :
-        // IE11対応（必須らしい）
-        ["web", "es5"]
-    }, assignPlugins(is_DEVELOP))
-  ]
+			module: {
+				rules: [{
+						test: /\.ejs$/,
+						use: 'ejs-compiled-loader',
+					},
+					{
+						test: /\.scss$/,
+						use: [
+							MiniCssExtractPlugin.loader,
+							{
+								loader: 'css-loader',
+								options: {
+									url: false, // sassで相対パスを書けるようにする
+									sourceMap: true,
+								},
+							},
+							{
+								loader: 'postcss-loader',
+								options: {
+									postcssOptions: {
+										plugins: [
+											[
+												require('autoprefixer')({
+													grid: true
+												}),
+											],
+										],
+									}
+								}
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+									implementation: require('sass'),
+								},
+							},
+						]
+					},
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						use: [{
+							loader: 'babel-loader',
+							options: {
+								// promiseを使えるようにするヤツ
+								presets: ['@babel/preset-env', '@babel/preset-react'],
+								plugins: ['@babel/plugin-transform-runtime'],
+							},
+						}, ],
+					},
+					{
+						test: /\.ts$/,
+						exclude: /node_modules/,
+						use: [{
+							loader: 'babel-loader',
+							options: {
+								presets: ['@babel/preset-typescript'],
+								plugins: ['@babel/proposal-class-properties'],
+							},
+						}, ],
+					},
+					{
+						enforce: 'pre',
+						test: /\.js$|\.ts$/,
+						exclude: /node_modules/,
+						loader: 'eslint-loader',
+					},
+				],
+			},
+			devServer: {
+				contentBase: path.resolve(__dirname, 'public'),
+				port: 8080,
+			},
+			stats: {
+				children: true //現状機能していない模様
+			},
+			target: is_DEVELOP ?
+				// dev-serverのホットリロードが効かない問題への対処
+				"web" :
+				// IE11対応（必須らしい）
+				["web", "es5"]
+		}, assignPlugins(is_DEVELOP))
+	]
 }
